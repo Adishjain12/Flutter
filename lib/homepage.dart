4cf4389eaa18b53e2b9ff051a25fdc6b678edce0
Adish Jain <adish12012002@gmail.com>
2021-12-31 16:28:44 +0530

    Day 2 Completed

---
+++ lib/homepage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class homepage extends StatelessWidget {
+  const homepage({Key? key}) : super(key: key);
+  final int days = 30;
+  final String name = "Adish";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+          child: Container(
+        child: Text(
+          "Flutter tutorial of $days days by $name",
+          style: TextStyle(fontSize: 20.0, color: Colors.amberAccent),
+        ),
+      )),
+      drawer: Drawer(),
+    );
+  }
+}
