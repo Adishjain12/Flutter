bbd66e39a0443160a97148a50b9c59668ffeecdd
Adish Jain <adish12012002@gmail.com>
2022-01-04 18:07:58 +0530

    Day 13 Completed

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,12 +1,35 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:loginapp/models/catalog.dart';
 import 'package:loginapp/widgets/drawer.dart';
 import 'package:loginapp/widgets/item_widget.dart';
 
-class homepage extends StatelessWidget {
+class homepage extends StatefulWidget {
   const homepage({Key? key}) : super(key: key);
+
+  @override
+  State<homepage> createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
   final int days = 30;
   final String name = "Adish";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData("products");
+    print(productsData);
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
