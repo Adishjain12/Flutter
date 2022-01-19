b0bcb240c9a8331b8b34b5e6242ae0ea6f18583e
Adish Jain <adish12012002@gmail.com>
2022-01-20 00:52:38 +0530

    Day 23 Completed

---
+++ lib/models/cart.dart
@@ -0,0 +1,33 @@
+import 'package:loginapp/models/catalog.dart';
+
+class CartModel {
+  //catalog field
+  late CatalogModel _catalog;
+
+  //collection of Ids -store Id of each item
+  final List<int> _itemIds = [];
+
+  //Get Catalog
+  CatalogModel get catalog => _catalog;
+
+  set catalog(CatalogModel newCatalog) {
+    _catalog = newCatalog;
+  }
+
+  //get items in the cart
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+  // Get total price
+  num get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+
+  //Add Item
+  void add(Item item) {
+    _itemIds.add(item.id);
+  }
+
+  //Remove Item
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+  }
+}
